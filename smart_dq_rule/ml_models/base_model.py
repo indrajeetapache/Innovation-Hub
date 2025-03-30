baf1fce2c1cba7301f690c279b4a3b07010169f9
indrajeetapache <indrajeetapache@gmail.com>
2025-03-30 17:40:10 +0000

    Add Smart DQ Rule system with data generator

---
+++ smart_dq_rule/ml_models/base_model.py
@@ -0,0 +1,14 @@
+class BaseModel(ABC):
+    """Abstract base class for all ML model interfaces."""
+    
+    @abstractmethod
+    def __init__(self, config: Dict[str, Any]):
+        pass
+    
+    @abstractmethod
+    def generate(self, prompt: str, **kwargs) -> str:
+        pass
+    
+    @abstractmethod
+    def analyze_column(self, column_name: str, column_metadata: Dict[str, Any]) -> List[Dict[str, Any]]:
+        pass
